d68a41c7dd825d34be88ca114c40ede0827fc0e0
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2022-05-06 17:02:35 +0100

    add lookup for title and metaDesc

---
+++ src/src.11tydata.js
@@ -0,0 +1,14 @@
+const i18n = require('./filters/i18n.js');
+
+module.exports = {
+  eleventyComputed: {
+    title: (data) => {
+      let str = i18n(data.title)
+      //console.log(str)
+      return str;
+    },
+    metaDesc: (data) => {
+      return i18n(data.metaDesc);
+    },
+  },
+};i18n(data.title)
